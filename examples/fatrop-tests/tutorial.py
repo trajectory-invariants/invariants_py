3f3c4404d7401c7eb247a24c26a844ae429056ad
Riccardo Burlizzi <riccardo.burlizzi@kuleuven.be>
2024-03-08 11:17:09 +0100

    In the tutorial can now set the desired solver and the type of trajectory

--- examples/fatrop-tests/tutorial.py
+++ examples/fatrop-tests/tutorial.py
@@ -10,12 +10,15 @@ import invariants_py.tutorial_scripts as tutorial
 
 # Set path to data file
 data_location = Path(__file__).resolve().parent.parent.parent / 'data' / 'beer_1.txt'
-
+# Set solver
+use_fatrop_solver = True
+# Set type of trajectory, options: "pose", "position", "rotation"
+traj_type = "pose"
 """
 Part 1: calculation invariants
 """
 # Calculate invariants
-invariants_calculation = tutorial.calculate_invariants(data_location)
+invariants_calculation = tutorial.calculate_invariants(data_location, use_fatrop_solver = use_fatrop_solver, traj_type = traj_type)
 
 
 
@@ -25,4 +28,4 @@ Part 2: generation new trajectories
 # Define target
 target = invariants_calculation.Obj_pos[-1]+[0.1,-0.1,-0.05]
 
-generated_trajectory = tutorial.generate_trajectory(data_location, invariants_calculation, target)
+generated_trajectory = tutorial.generate_trajectory(data_location, invariants_calculation, target, use_fatrop_solver, traj_type = traj_type)
--- invariants_py/tutorial_scripts.py
+++ invariants_py/tutorial_scripts.py
@@ -8,7 +8,6 @@ Created on Fri Feb 9 2024
 # Imports
 import numpy as np
 from math import pi
-import os
 import invariants_py.read_and_write_data as rw
 import matplotlib.pyplot as plt
 import invariants_py.reparameterization as reparam
@@ -21,8 +20,6 @@ from IPython.display import clear_output
 from scipy.spatial.transform import Rotation as R
 from invariants_py.robotics_functions.orthonormalize_rotation import orthonormalize_rotation as orthonormalize
 import invariants_py.plotters as pl
-import random
-import invariants_py.robotics_functions.collision_detection as cd
 from invariants_py.reparameterization import interpR
 from invariants_py.FSr_init import FSr_init
 
@@ -36,7 +33,7 @@ class OCP_results:
         self.Obj_frames = Obj_frames
         self.invariants = invariants
 #%%
-def calculate_invariants(data_location, plot_demo = True, use_fatrop_solver = False, plot_inv = True):
+def calculate_invariants(data_location, plot_demo = True, use_fatrop_solver = False, plot_inv = True, traj_type = "position"):
     trajectory,time = rw.read_pose_trajectory_from_txt(data_location)
     pose,time_profile,arclength,nb_samples,stepsize = reparam.reparameterize_trajectory_arclength(trajectory)
     arclength_n = arclength/arclength[-1]
@@ -62,8 +59,15 @@ def calculate_invariants(data_location, plot_demo = True, use_fatrop_solver = Fa
     FS_calculation_problem_rot = FS_calc_rot(window_len=nb_samples, bool_unsigned_invariants = False, rms_error_traj = 2*pi/180, fatrop_solver = use_fatrop_solver) 
 
     # calculate invariants given measurements
-    optim_calc_results.invariants[:,3:], optim_calc_results.Obj_pos, optim_calc_results.FSt_frames = FS_calculation_problem_pos.calculate_invariants_global(trajectory,stepsize)
-    optim_calc_results.invariants[:,:3], optim_calc_results.Obj_frames, optim_calc_results.FSr_frames = FS_calculation_problem_rot.calculate_invariants_global(trajectory,stepsize)
+    if not traj_type == "position":
+        optim_calc_results.invariants[:,:3], optim_calc_results.Obj_frames, optim_calc_results.FSr_frames = FS_calculation_problem_rot.calculate_invariants_global(trajectory,stepsize)
+        if traj_type == "pose":
+            optim_calc_results.invariants[:,3:], optim_calc_results.Obj_pos, optim_calc_results.FSt_frames = FS_calculation_problem_pos.calculate_invariants_global(trajectory,stepsize)
+        else:
+            optim_calc_results.Obj_pos = trajectory_position
+    else:
+        optim_calc_results.invariants, optim_calc_results.Obj_pos, optim_calc_results.FSt_frames = FS_calculation_problem_pos.calculate_invariants_global(trajectory,stepsize)
+        optim_calc_results.Obj_frames = trajectory_orientation
 
     if plot_inv:
         fig = plt.figure(figsize=(8,8))
@@ -73,19 +77,24 @@ def calculate_invariants(data_location, plot_demo = True, use_fatrop_solver = Fa
         ax.plot(optim_calc_results.Obj_pos[:,0],optim_calc_results.Obj_pos[:,1],optim_calc_results.Obj_pos[:,2],'r')
         indx = np.trunc(np.linspace(0,len(optim_calc_results.Obj_pos)-1,n_frames))
         indx = indx.astype(int)
-        for i in indx:
-            pl.plot_3d_frame(optim_calc_results.Obj_pos[i,:],optim_calc_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
+        if not traj_type == "position":
+            for i in indx:
+                pl.plot_3d_frame(optim_calc_results.Obj_pos[i,:],optim_calc_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
 
-        pl.plot_orientation(optim_calc_results.Obj_frames,trajectory_orientation)
-
-        pl.plot_invariants(optim_calc_results.invariants,[],arclength_n)
+            pl.plot_orientation(optim_calc_results.Obj_frames,trajectory_orientation)
+        if traj_type == "pose":
+            pl.plot_invariants(optim_calc_results.invariants,[],arclength_n)
+        elif traj_type == "position":
+            pl.plot_invariants(optim_calc_results.invariants,[],arclength_n, inv_type = "FS_pos")
+        else:
+            pl.plot_invariants(optim_calc_results.invariants,[],arclength_n, inv_type = "FS_rot")
 
     plt.show()
 
     return optim_calc_results
 
 #%%
-def generate_trajectory(data_location, optim_calc_results, p_obj_end, use_fatrop_solver = False, plot_new_trajectory = True):
+def generate_trajectory(data_location, optim_calc_results, p_obj_end, use_fatrop_solver = False, plot_new_trajectory = True, traj_type = "position"):
     trajectory,time = rw.read_pose_trajectory_from_txt(data_location)
     pose,time_profile,arclength,nb_samples,stepsize = reparam.reparameterize_trajectory_arclength(trajectory)
     arclength_n = arclength/arclength[-1]
@@ -114,13 +123,12 @@ def generate_trajectory(data_location, optim_calc_results, p_obj_end, use_fatrop
     current_index = round(current_progress*len(trajectory))
     p_obj_start = optim_calc_results.Obj_pos[current_index]
     R_obj_start = orthonormalize(optim_calc_results.Obj_frames[current_index])
-    FSt_start = orthonormalize(optim_calc_results.FSt_frames[current_index])
-    # FSr_start = orthonormalize(optim_calc_results.FSr_frames[current_index])
-    alpha = 0
+    alpha = 180
     rotate = R.from_euler('z', alpha, degrees=True)
     R_obj_end =  orthonormalize(rotate.as_matrix() @ optim_calc_results.Obj_frames[-1])
-    FSt_end = orthonormalize(rotate.as_matrix() @ optim_calc_results.FSt_frames[-1])
-    # FSr_end = orthonormalize(optim_calc_results.FSr_frames[-1])
+    if not traj_type == "rotation":
+        FSt_start = orthonormalize(optim_calc_results.FSt_frames[current_index])
+        FSt_end = orthonormalize(rotate.as_matrix() @ optim_calc_results.FSt_frames[-1])
 
     # define new class for OCP results
     optim_gen_results = OCP_results(FSt_frames = [], FSr_frames = [], Obj_pos = [], Obj_frames = [], invariants = np.zeros((number_samples,6)))
@@ -131,17 +139,26 @@ def generate_trajectory(data_location, optim_calc_results, p_obj_end, use_fatrop
 
     # Linear initialization
     R_obj_init = interpR(np.linspace(0, 1, len(optim_calc_results.Obj_frames)), [0,1], np.array([R_obj_start, R_obj_end]))
-    # R_r_init = interpR(np.linspace(0, 1, len(optim_calc_results.FSr_frames)), [0,1], np.array([FSr_start, FSr_end]))
-
-    R_r_init, R_r_init_array, U_init = FSr_init(R_obj_start, R_obj_end)
+    
+    if not traj_type == "position":
+        R_r_init, R_r_init_array, U_init = FSr_init(R_obj_start, R_obj_end)
 
     # Define OCP weights
     w_invars_pos = np.array([5*10**1, 1.0, 1.0])
     w_invars_rot = 10**2*np.array([10**1, 1.0, 1.0])
 
     # Solve
-    optim_gen_results.invariants[:,3:], optim_gen_results.Obj_pos, optim_gen_results.FSt_frames, tot_time_pos = FS_online_generation_problem_pos.generate_trajectory(U_demo = model_invariants[:,3:], p_obj_init = optim_calc_results.Obj_pos, R_t_init = optim_calc_results.FSt_frames, R_t_start = FSt_start, R_t_end = FSt_end, p_obj_start = p_obj_start, p_obj_end = p_obj_end, step_size = new_stepsize, w_invars = w_invars_pos)
-    optim_gen_results.invariants[:,:3], optim_gen_results.Obj_frames, optim_gen_results.FSr_frames, tot_time_rot = FS_online_generation_problem_rot.generate_trajectory(U_demo = model_invariants[:,:3]*0., U_init = U_init, R_obj_init = R_obj_init, R_r_init = R_r_init_array, R_r_start = R_r_init, R_r_end = R_r_init, R_obj_start = R_obj_start, R_obj_end = R_obj_end, step_size = new_stepsize)
+    if not traj_type == "position":
+        optim_gen_results.invariants[:,:3], optim_gen_results.Obj_frames, optim_gen_results.FSr_frames, tot_time_rot = FS_online_generation_problem_rot.generate_trajectory(U_demo = model_invariants[:,:3]*0., U_init = U_init, R_obj_init = R_obj_init, R_r_init = R_r_init_array, R_r_start = R_r_init, R_r_end = R_r_init, R_obj_start = R_obj_start, R_obj_end = R_obj_end, step_size = new_stepsize)
+        if traj_type == "pose":
+            optim_gen_results.invariants[:,3:], optim_gen_results.Obj_pos, optim_gen_results.FSt_frames, tot_time_pos = FS_online_generation_problem_pos.generate_trajectory(U_demo = model_invariants[:,3:], p_obj_init = optim_calc_results.Obj_pos, R_t_init = optim_calc_results.FSt_frames, R_t_start = FSt_start, R_t_end = FSt_end, p_obj_start = p_obj_start, p_obj_end = p_obj_end, step_size = new_stepsize, w_invars = w_invars_pos)
+        else:
+            optim_gen_results.Obj_pos = pose[:,:3,3]
+            tot_time_pos = 0
+    else:
+        optim_gen_results.invariants, optim_gen_results.Obj_pos, optim_gen_results.FSt_frames, tot_time_pos = FS_online_generation_problem_pos.generate_trajectory(U_demo = model_invariants, p_obj_init = optim_calc_results.Obj_pos, R_t_init = optim_calc_results.FSt_frames, R_t_start = FSt_start, R_t_end = FSt_end, p_obj_start = p_obj_start, p_obj_end = p_obj_end, step_size = new_stepsize, w_invars = w_invars_pos)
+        optim_gen_results.Obj_frames = pose[:,:3,:3]
+        tot_time_rot = 0
     print('')
     print("TOTAL time to generate new trajectory: ")
     print(str(tot_time_pos + tot_time_rot) + "[s]")
@@ -157,11 +174,17 @@ def generate_trajectory(data_location, optim_calc_results, p_obj_end, use_fatrop
 
         indx_online = np.trunc(np.linspace(0,len(optim_gen_results.Obj_pos)-1,n_frames))
         indx_online = indx_online.astype(int)
-        for i in indx_online:
-            pl.plot_3d_frame(optim_calc_results.Obj_pos[i,:],optim_calc_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
-            pl.plot_3d_frame(optim_gen_results.Obj_pos[i,:],optim_gen_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
-        pl.plot_orientation(optim_calc_results.Obj_frames,optim_gen_results.Obj_frames,current_index)
+        if not traj_type == "position":
+            for i in indx_online:
+                pl.plot_3d_frame(optim_calc_results.Obj_pos[i,:],optim_calc_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
+                pl.plot_3d_frame(optim_gen_results.Obj_pos[i,:],optim_gen_results.Obj_frames[i,:,:],1,0.01,['red','green','blue'],ax)
+            pl.plot_orientation(optim_calc_results.Obj_frames,optim_gen_results.Obj_frames,current_index)
 
-        pl.plot_invariants(optim_calc_results.invariants, optim_gen_results.invariants, arclength_n, progress_values)
+        if traj_type == "pose":
+            pl.plot_invariants(optim_calc_results.invariants, optim_gen_results.invariants, arclength_n, progress_values)
+        elif traj_type == "position":
+            pl.plot_invariants(optim_calc_results.invariants, optim_gen_results.invariants, arclength_n, progress_values, 'FS_pos')
+        else:
+            pl.plot_invariants(optim_calc_results.invariants, optim_gen_results.invariants, arclength_n, progress_values, 'FS_rot')
 
     plt.show()
