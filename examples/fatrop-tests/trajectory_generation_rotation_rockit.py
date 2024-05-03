Finished ocp_to_function for rotation, working example; missing online plot and fix full pose example

examples/fatrop-tests/trajectory_generation_rotation_rockit.py
@@ -105,19 +105,23 @@ R_obj_end =  orthonormalize(rotate.as_matrix() @ calculate_trajectory[-1])
 # R_r_start = orthonormalize(movingframes[current_index])
 # R_r_end = orthonormalize(movingframes[-1])
 
-
-# specify optimization problem symbolically
-FS_online_generation_problem = FS_gen(window_len=number_samples, fatrop_solver = use_fatrop_solver)
-
 # Linear initialization
 R_obj_init = interpR(np.linspace(0, 1, len(calculate_trajectory)), [0,1], np.array([R_obj_start, R_obj_end]))
 
 R_r_init, R_r_init_array, invars_init = FSr_init(R_obj_start, R_obj_end)
 
-w_invars_rot = 10**2*np.array([10**1, 1.0, 1.0])
+initial_values = {"invariants": invars_init, "trajectory": R_obj_init, "moving-frame": R_r_init_array}
+
+weight_params = {"w_invars": 10**2*np.array([10**1, 1.0, 1.0])}
+
+boundary_constraints = {"orientation": {"initial": R_obj_start, "final": R_obj_end}, "moving-frame": {"initial": R_r_init, "final": R_r_init}}
+
+# specify optimization problem symbolically
+FS_online_generation_problem = FS_gen(boundary_constraints, number_samples, fatrop_solver = use_fatrop_solver)
 
 # Solve
-new_invars, new_trajectory, new_movingframes, tot_time_rot = FS_online_generation_problem.generate_trajectory(invars_demo = model_invariants*0., invars_init = invars_init, R_obj_init = R_obj_init, R_r_init = R_r_init_array, R_r_start = R_r_init, R_r_end = R_r_init, R_obj_start = R_obj_start, R_obj_end = R_obj_end, step_size = new_stepsize)
+new_invars, new_trajectory, new_movingframes, tot_time_rot = FS_online_generation_problem.generate_trajectory(model_invariants,boundary_constraints,new_stepsize,weight_params,initial_values)
+
 if use_fatrop_solver:
     print('')
     print("TOTAL time to generate new trajectory: ")
@@ -158,19 +162,20 @@ if plt.get_backend() != 'agg':
 window_len = 20
 
 # specify optimization problem symbolically
-FS_online_generation_problem = FS_gen(window_len=window_len, fatrop_solver = use_fatrop_solver)
+FS_online_generation_problem2 = FS_gen(boundary_constraints, window_len, fatrop_solver = use_fatrop_solver)
 
 current_progress = 0.0
 old_progress = 0.0
 
-R_obj_end = calculate_trajectory[-1] # initialise R_obj_end with end point of reconstructed trajectory
-iterative_trajectory = calculate_trajectory.copy()
-iterative_movingframes = movingframes.copy()
+# plt.ion()  # Interactive plotting
+# fig_traj = plt.figure(figsize=(8, 8))
+# fig_invars = plt.figure(figsize=(12, 4))
+
+R_obj_end = calculate_trajectory[-1]
 trajectory_position_iter = trajectory_position.copy()
 fig_invars = plt.figure(figsize=(10, 6))
 fig = plt.figure(figsize=(14,8))
 ax = fig.add_subplot(111, projection='3d')
-
 while current_progress <= 1.0:
     
     print(f"current progress = {current_progress}")
@@ -180,43 +185,54 @@ while current_progress <= 1.0:
     model_invariants,new_stepsize = interpolate_model_invariants(spline_model_trajectory,progress_values)
     
     # Boundary constraints
-    current_index = round( (current_progress - old_progress) * len(iterative_trajectory))
-    R_obj_start = iterative_trajectory[current_index]
+    current_index = round( (current_progress - old_progress) * len(calculate_trajectory))
     rotate = R.from_euler('z', 30/window_len, degrees=True)
     R_obj_end =  orthonormalize(rotate.apply(R_obj_end))
-    R_r_start = iterative_movingframes[current_index] 
-    R_r_end = iterative_movingframes[-1] 
+    boundary_constraints = {"orientation": {"initial": calculate_trajectory[current_index], "final": R_obj_end}, "moving-frame": {"initial": movingframes[current_index], "final": movingframes[-1]}}
+
+    initial_values = {"invariants": model_invariants, "trajectory": calculate_trajectory, "moving-frame": movingframes}
 
     # Calculate remaining trajectory
-    new_invars, iterative_trajectory, iterative_movingframes, tot_time_rot = FS_online_generation_problem.generate_trajectory(invars_demo = model_invariants, R_obj_init = calculate_trajectory, R_r_init = movingframes, R_r_start = R_r_start, R_r_end = R_r_end, R_obj_start = R_obj_start, R_obj_end = R_obj_end, step_size = new_stepsize, w_invars = w_invars_rot)
+    new_invars, calculate_trajectory, movingframes, tot_time_rot = FS_online_generation_problem2.generate_trajectory_OLD(model_invariants,boundary_constraints,new_stepsize,weight_params,initial_values)
     if use_fatrop_solver:
         print('')
         print("TOTAL time to generate new trajectory: ")
         print(str(tot_time_rot) + '[s]')
 
-    for i in range(len(iterative_trajectory)):
-        iterative_trajectory[i] = orthonormalize(iterative_trajectory[i])
+    # fig_traj.clf()
+    # ax_traj = fig_traj.add_subplot(111, projection='3d')
+    # ax_traj.view_init(elev=26, azim=-40)
+    # ax_traj.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], 'b')
+    # ax_traj.plot(calculate_trajectory[:, 0], calculate_trajectory[:, 1], calculate_trajectory[:, 2], 'r')
+    # fig_traj.canvas.draw()
+    # fig_traj.canvas.flush_events()
 
-    # Visualization
-    clear_output(wait=True)
+
+    for i in range(len(calculate_trajectory)):
+        calculate_trajectory[i] = orthonormalize(calculate_trajectory[i])
+
+    # NEED TO WORK ON THE VISUALIZATION!!!####################################
+    
+    # # Visualization
+    # clear_output(wait=True)
     
 
-    ax.plot(trajectory_position[:,0],trajectory_position[:,1],trajectory_position[:,2],'b')
-    for i in indx:
-        plot_stl(opener_location,trajectory_position[i,:],calculate_trajectory[i,:,:],colour="c",alpha=0.2,ax=ax)
-    cut_trajectory_position = trajectory_position_iter[current_index:,:]
-    interp = ip.interp1d(np.linspace(0,1,len(cut_trajectory_position)),cut_trajectory_position, axis = 0)
-    trajectory_position_iter = interp(np.linspace(0,1,len(iterative_trajectory)))
-    indx_iter = np.trunc(np.linspace(0,len(iterative_trajectory)-1,n_frames))
-    indx_iter = indx_iter.astype(int)
-    for i in indx_iter:
-        plot_3d_frame(trajectory_position_iter[i,:],iterative_trajectory[i,:,:],1,0.05,['red','green','blue'],ax)
-        plot_stl(opener_location,trajectory_position_iter[i,:],iterative_trajectory[i,:,:],colour="r",alpha=0.2,ax=ax)
+    # ax.plot(trajectory_position[:,0],trajectory_position[:,1],trajectory_position[:,2],'b')
+    # for i in indx:
+    #     plot_stl(opener_location,trajectory_position[i,:],calculate_trajectory[i,:,:],colour="c",alpha=0.2,ax=ax)
+    # cut_trajectory_position = trajectory_position_iter[current_index:,:]
+    # interp = ip.interp1d(np.linspace(0,1,len(cut_trajectory_position)),cut_trajectory_position, axis = 0)
+    # trajectory_position_iter = interp(np.linspace(0,1,len(calculate_trajectory)))
+    # indx_iter = np.trunc(np.linspace(0,len(calculate_trajectory)-1,n_frames))
+    # indx_iter = indx_iter.astype(int)
+    # for i in indx_iter:
+    #     plot_3d_frame(trajectory_position_iter[i,:],calculate_trajectory[i,:,:],1,0.05,['red','green','blue'],ax)
+    #     plot_stl(opener_location,trajectory_position_iter[i,:],calculate_trajectory[i,:,:],colour="r",alpha=0.2,ax=ax)
     
-    pl.plot_invariants(invariants,new_invars,arclength_n,progress_values,inv_type='FS_rot',fig=fig_invars)
+    # pl.plot_invariants(invariants,new_invars,arclength_n,progress_values,inv_type='FS_rot',fig=fig_invars)
 
-    if plt.get_backend() != 'agg':
-        plt.show()
+    # if plt.get_backend() != 'agg':
+    #     plt.show()
     
     old_progress = current_progress
     current_progress = old_progress + 1/window_len

invariants_py/rockit_generate_rotation_from_vector_invariants.py
@@ -120,6 +120,10 @@ class OCP_gen_rot:
         if "orientation" in boundary_constraints and "final" in boundary_constraints["orientation"]:
             ocp.set_value(R_obj_end, rotate_x(np.pi/4))
         ocp.solve_limited() # code generation
+        if fatrop_solver:
+            tot_time = ocp._method.myOCP.get_stats().time_total
+        else:
+            tot_time = 0
 
         self.first_window = True
 
@@ -161,34 +165,60 @@ class OCP_gen_rot:
         )
 
         # Save variables
-        # self.R_r_x = R_r_x
-        # self.R_r_y = R_r_y
-        # self.R_r_z = R_r_z
-        # self.R_r = R_r
-        # self.R_obj_x = R_obj_x
-        # self.R_obj_y = R_obj_y
-        # self.R_obj_z = R_obj_z
-        # self.R_obj = R_obj
-        # self.invars = invars
-        # self.invars_demo = invars_demo
-        # self.w_invars = w_invars
-        # if bound_mf:
-        #     self.R_r_start = R_r_start
-        #     self.R_r_end = R_r_end
-        # self.R_obj_start = R_obj_start
-        # self.R_obj_end = R_obj_end
-        # self.h = h
-        # self.window_len = window_len
-        # self.ocp = ocp
-        # self.fatrop = fatrop_solver
+        self.R_r_x = R_r_x
+        self.R_r_y = R_r_y
+        self.R_r_z = R_r_z
+        self.R_r = R_r
+        self.R_obj_x = R_obj_x
+        self.R_obj_y = R_obj_y
+        self.R_obj_z = R_obj_z
+        self.R_obj = R_obj
+        self.invars = invars
+        self.invars_demo = invars_demo
+        self.w_invars = w_invars
+        if "moving-frame" in boundary_constraints and "initial" in boundary_constraints["moving-frame"]:
+            self.R_r_start = R_r_start
+        if "moving-frame" in boundary_constraints and "final" in boundary_constraints["moving-frame"]:
+            self.R_r_end = R_r_end
+        if "orientation" in boundary_constraints and "initial" in boundary_constraints["orientation"]:
+            self.R_obj_start = R_obj_start
+        if "orientation" in boundary_constraints and "final" in boundary_constraints["orientation"]:
+            self.R_obj_end = R_obj_end
+        self.h = h
+        self.window_len = window_len
+        self.ocp = ocp
+        self.sol = None
+        self.first_window = True
+        self.fatrop = fatrop_solver
+        self.tot_time = tot_time
         
          
-    def generate_trajectory(self,invars_demo,R_obj_init,R_r_init,R_r_start,R_r_end,R_obj_start,R_obj_end,step_size, invars_init=None,w_invars = (10**-3)*np.array([1.0, 1.0, 1.0]), w_high_start = 1, w_high_end = 0, w_high_invars = (10**-3)*np.array([1.0, 1.0, 1.0]), w_high_active = 0):
-        #%%
-      
-        if invars_init is None:
-            invars_init = invars_demo
+    def generate_trajectory_OLD(self,invariant_model,boundary_constraints, step_size, weights_params,initial_values):
+
+        N = np.size(invariant_model,0)
         
+        R_obj_init = initial_values['trajectory']
+        R_r_init = initial_values['moving-frame']
+        invars_init = initial_values['invariants']
+
+        R_r_start = boundary_constraints["moving-frame"]["initial"]
+        R_r_end = boundary_constraints["moving-frame"]["final"]
+        R_obj_start = boundary_constraints["orientation"]["initial"]
+        R_obj_end = boundary_constraints["orientation"]["final"]
+
+        # Get the weights for the invariants or set default values
+        w_invars = weights_params.get('w_invars', (10**-3)*np.array([1.0, 1.0, 1.0]))
+        w_high_start = weights_params.get('w_high_start', 1)
+        w_high_end = weights_params.get('w_high_end', 0)
+        w_high_invars = weights_params.get('w_high_invars', (10**-3)*np.array([1.0, 1.0, 1.0]))
+        w_high_active = weights_params.get('w_high_active', 0)
+
+        # Set the weights for the invariants
+        weights = np.tile(w_invars, (len(invariant_model), 1))
+        if w_high_active:
+            weights[w_high_start:w_high_end+1, :] = w_high_invars
+        self.ocp.set_value(self.w_invars, weights.T) 
+
         # Initialize states
         self.ocp.set_initial(self.R_obj_x, R_obj_init[:self.window_len,:,0].T) 
         self.ocp.set_initial(self.R_obj_y, R_obj_init[:self.window_len,:,1].T) 
@@ -198,38 +228,25 @@ class OCP_gen_rot:
         self.ocp.set_initial(self.R_r_z, R_r_init[:self.window_len,:,2].T) 
             
         # Initialize controls
-        self.ocp.set_initial(self.invars,invars_init[:-1,:].T)
+        self.ocp.set_initial(self.invars,invars_init.T)
 
         # Set values boundary constraints
         self.ocp.set_value(self.R_r_start,R_r_start)
         self.ocp.set_value(self.R_r_end,R_r_end)
         self.ocp.set_value(self.R_obj_start,R_obj_start)
         self.ocp.set_value(self.R_obj_end,R_obj_end)
-                
+
         # Set values parameters
         self.ocp.set_value(self.h,step_size)
-        self.ocp.set_value(self.invars_demo, invars_demo.T)     
-        weights = np.zeros((len(invars_demo),3))
-        if w_high_active:
-            for i in range(len(invars_demo)):
-                if i >= w_high_start and i <= w_high_end:
-                    weights[i,:] = w_high_invars
-                else:
-                    weights[i,:] = w_invars
-        else:
-            for i in range(len(invars_demo)):
-                weights[i,:] = w_invars
-        self.ocp.set_value(self.w_invars, weights.T) 
+        self.ocp.set_value(self.invars_demo, invariant_model.T)  
 
         # Solve the NLP
         sol = self.ocp.solve()
-        if self.fatrop:
-            tot_time = self.ocp._method.myOCP.get_stats().time_total
-        else:
-            tot_time = 0
-        
-        self.sol = sol
-              
+        # if self.fatrop:
+        #     tot_time = self.ocp._method.myOCP.get_stats().time_total
+        # else:
+        #     tot_time = 0
+                      
         # Extract the solved variables
         _,i_r1 = sol.sample(self.invars[0],grid='control')
         _,i_r2 = sol.sample(self.invars[1],grid='control')
@@ -238,21 +255,36 @@ class OCP_gen_rot:
         _,calculated_trajectory = sol.sample(self.R_obj,grid='control')
         _,calculated_movingframe = sol.sample(self.R_r,grid='control')
                 
-        return invariants, calculated_trajectory, calculated_movingframe, tot_time
+        return invariants, calculated_trajectory, calculated_movingframe, self.tot_time
     
-    def generate_trajectory_online(self, invariant_model, boundary_constraints, step_size):
+    def generate_trajectory(self, invariant_model, boundary_constraints, step_size, weights_params = {}, initial_values = {}):
 
-        invars_demo = invariant_model
-        w_invars = (10**-3)*np.array([1.0, 1.0, 1.0])
+        N = invariant_model.shape[0]
+
+        # Get the weights for the invariants or set default values
+        w_invars = weights_params.get('w_invars', (10**-3)*np.array([1.0, 1.0, 1.0]))
+        w_high_start = weights_params.get('w_high_start', 1)
+        w_high_end = weights_params.get('w_high_end', 0)
+        w_high_invars = weights_params.get('w_high_invars', (10**-3)*np.array([1.0, 1.0, 1.0]))
+        w_high_active = weights_params.get('w_high_active', 0)
+
+        # Set the weights for the invariants
+        w_invars = np.tile(w_invars, (len(invariant_model),1)).T
+        if w_high_active:
+            w_invars[:, w_high_start:w_high_end+1] = w_high_invars.reshape(-1, 1)
 
         boundary_values_list = [value for sublist in boundary_constraints.values() for value in sublist.values()]
 
-        if self.first_window:
-            self.solution = generate_initvals_from_bounds_rot(boundary_constraints, np.size(invars_demo,0))
+        if self.first_window and not initial_values:
+            self.solution = generate_initvals_from_bounds_rot(boundary_constraints, np.size(invariant_model,0))
+            self.first_window = False
+        elif self.first_window:
+            self.solution = [initial_values["invariants"][:N-1,:].T, initial_values["moving-frame"][:N,:,0].T, initial_values["moving-frame"][:N,:,1].T, initial_values["moving-frame"][:N,:,2].T, initial_values["trajectory"][:N,:,0].T, initial_values["trajectory"][:N,:,1].T, initial_values["trajectory"][:N,:,2].T]
             self.first_window = False
 
+
         # Call solve function
-        self.solution = self.ocp_function(invars_demo.T,w_invars,step_size,*boundary_values_list,*self.solution)
+        self.solution = self.ocp_function(invariant_model.T,w_invars,step_size,*boundary_values_list,*self.solution)
 
         #Return the results
         invars_sol, R_r_x_sol, R_r_y_sol, R_r_z_sol, R_obj_x_sol, R_obj_y_sol, R_obj_z_sol = self.solution # unpack the results            
@@ -261,7 +293,7 @@ class OCP_gen_rot:
         calculated_trajectory = np.reshape(np.hstack((R_obj_x_sol[:], R_obj_y_sol[:], R_obj_z_sol[:])), (-1,3,3)) # make a N x 3 x 3 array
         calculated_movingframe = np.reshape(np.hstack((R_r_x_sol[:], R_r_y_sol[:], R_r_z_sol[:])), (-1,3,3)) # make a N x 3 x 3 array
 
-        return invariants, calculated_trajectory, calculated_movingframe
+        return invariants, calculated_trajectory, calculated_movingframe, self.tot_time
     
 if __name__ == "__main__":
 
@@ -286,7 +318,7 @@ if __name__ == "__main__":
     ocp = OCP_gen_rot(boundary_constraints,fatrop_solver=True, window_len=N)
 
     # Call the generate_trajectory function
-    invariants, calculated_trajectory, calculated_movingframe = ocp.generate_trajectory_online(invariant_model, boundary_constraints, step_size)
+    invariants, calculated_trajectory, calculated_movingframe = ocp.generate_trajectory(invariant_model, boundary_constraints, step_size)
 
     # Print the results
     print("Invariants_start:", invariants[0])
@@ -300,7 +332,7 @@ if __name__ == "__main__":
     # Second call to generate_trajectory_online
     boundary_constraints["orientation"]["initial"] = boundary_constraints["orientation"]["final"]
     boundary_constraints["orientation"]["final"] = rotate_x(np.pi/8)
-    invariants, calculated_trajectory, calculated_movingframe = ocp.generate_trajectory_online(invariant_model, boundary_constraints, step_size)
+    invariants, calculated_trajectory, calculated_movingframe = ocp.generate_trajectory(invariant_model, boundary_constraints, step_size)
 
     # Print the results
     print("Invariants_start:", invariants[0])