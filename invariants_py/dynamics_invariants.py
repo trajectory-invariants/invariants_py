a2af16a3b8daa8207041f9bc970f00feffd46260
Maxim Vochten <maxim.vochten@kuleuven.be>
2024-06-05 16:26:37 +0200

    start specification screw invariants

--- invariants_py/dynamics_invariants.py
+++ invariants_py/dynamics_invariants.py
@@ -17,7 +17,6 @@ def rodriguez_rot_form(omega,h):
 #    return cas.SX.eye(3) + cas.mtimes(cas.sin(theta),K) + (1-cas.cos(theta))*cas.mtimes(K,K)
 
 def rodrigues2(skewer):
-    
     omega = cas.vertcat(-skewer[1,2],skewer[0,2],-skewer[0,1])
     return rodriguez_rot_form(omega,h=1)
 
@@ -219,6 +218,51 @@ def define_geom_integrator_tra_FSI_casadi(h):
     out_plus1 = cas.vertcat(cas.vec(R_t_plus1),  p_obj_plus1)
     integrator = cas.Function("phi", [x,u,h] , [out_plus1])
     
+    return integrator
+
+
+def geo_integrator_ISA(T_isa, T_obj, u, h):
+    """Integrate invariants over interval h starting from a current state (object pose + moving frames)"""
+    # Define a geometric integrator for eFSI,
+    # (meaning rigid-body motion is perfectly integrated assuming constant invariants)
+    i1 = u[0]
+    i2 = u[1]
+    i3 = u[2]
+    i4 = u[3]
+    i5 = u[4]
+    i6 = u[5]
+
+    invariants_isa = cas.vertcat(i3,i2,0,i6,i5,0)
+    invariants_obj = cas.vertcat(i1,0,0,i4,0,0)
+
+    omega_o = cas.mtimes(T_isa, cas.vertcat(i1,0,0))
+
+    #translation
+    deltaR_t = rodriguez_rot_form(omega_t,h)
+    T_isa_plus1 = cas.mtimes(T_isa,deltaR_t)
+    p_obj_plus1 = cas.mtimes(T_isa, cas.vertcat(i4,0,0))*h + T_obj[:,3]
+
+    #rotation
+    deltaR_r = rodriguez_rot_form(omega_r,h)
+    T_obj_plus1 = cas.mtimes(T_obj,deltaR_r)
+
+    deltaR_o = rodriguez_rot_form(omega_o,h)
+    T_obj_plus1 = cas.mtimes(deltaR_o,T_obj_plus1)
+
+    return (T_isa_plus1, T_obj_plus1)
+
+def dynamics_screw_invariants_pose_traj(h):
+    # System states
+    T_isa  = cas.MX.sym('R_r',3,4) 
+    T_obj = cas.MX.sym('R_obj',3,4)
+    x = cas.vertcat(cas.vec(T_isa), cas.vec(T_obj))
+
+    u = cas.MX.sym('i',6,1)
+
+    (T_isa_plus1, T_obj_plus1) = geo_integrator_ISA(T_isa, T_obj, u, h)
+    out_plus1 = cas.vertcat(cas.vec(T_isa_plus1),  cas.vec(T_obj_plus1))
+    integrator = cas.Function("phi", [x,u,h] , [out_plus1])
+
     return integrator
 
 def define_geom_integrator_rot_FSI_casadi(h):
---
+++ invariants_py/opti_calculate_screw_invariants_pose.py
@@ -0,0 +1,66 @@
+import numpy as np
+import casadi as cas
+from invariants_py.dynamics_invariants import dynamics_screw_invariants_pose_traj
+from invariants_py import ocp_helper
+
+class OCP_calc_pose:    
+
+    def __init__(self, N = 100, bool_unsigned_invariants = False, rms_error_traj = 10**-2):
+       
+        opti = cas.Opti() # use OptiStack package from Casadi for easy bookkeeping of variables (no cumbersome indexing)
+        
+        # Define system states X (unknown object pose + moving frame pose at every time step)
+        T_isa = [opti.variable(3,4) for _ in range(N)]  # Instantaneous Screw Axis frame
+        T_obj = [opti.variable(3,4) for _ in range(N)]  # Object frame
+        X = [cas.vertcat(cas.vec(T_isa[k]), cas.vec(T_obj[k])) for k in range(N)]
+
+        # Define system controls U (invariants at every time step)
+        U = opti.variable(6,N-1)
+
+        # Define system parameters P (known values in optimization that need to be set right before solving)
+        T_obj_m = [opti.parameter(3,4) for _ in range(N)] # measured object poses
+        h = opti.parameter() # step size for integration of dynamic equations
+    
+        # Constrain rotation matrices to be orthogonal (only needed for one timestep, property is propagated by integrator)
+        opti.subject_to(ocp_helper.tril_vec(T_isa[0][0:3,0:3].T @ T_isa[0][0:3,0:3] - np.eye(3)) == 0)
+        opti.subject_to(ocp_helper.tril_vec(T_obj[0][0:3,0:3].T @ T_obj[0][0:3,0:3] - np.eye(3)) == 0)
+
+        # Dynamics constraints (Multiple shooting)
+        integrator = dynamics_screw_invariants_pose_traj(h)
+        for k in range(N-1):
+            # Integrate current state to obtain next state (next rotation and position)
+            Xk_end = integrator(X[k],U[:,k],h)
+            
+            # Continuity constraint (closing the gap)
+            opti.subject_to(Xk_end==X[k+1])
+            
+        # Lower bounds on controls
+        if bool_unsigned_invariants:
+            opti.subject_to(U[0,:] >= 0) # lower bounds on control
+            opti.subject_to(U[1,:] >= 0) # lower bounds on control
+
+        # Measurement fitting constraint
+        trajectory_error_pos = 0
+        trajectory_error_rot = 0
+        for k in range(N):
+            err_pos = T_obj[k][0:3,3] - T_obj_m[k][0:3,3] # position error
+            err_rot = T_obj_m[k][0:3,0:3].T @ T_obj[k][0:3,0:3] - np.eye(3) # orientation error
+            trajectory_error_pos = trajectory_error_pos + cas.dot(err_pos,err_pos)
+            trajectory_error_rot = trajectory_error_rot + cas.dot(err_rot,err_rot)  
+        opti.subject_to(trajectory_error_pos/N/rms_error_traj**2 < 1)
+        opti.subject_to(trajectory_error_rot/N/rms_error_traj**2 < 1)
+
+        # Minimize moving frame invariants to deal with singularities and noise
+        objective_reg = 0
+        for k in range(N-1):
+            err_abs = U[[1,2,4,5],k] # value of moving frame invariants
+            objective_reg = objective_reg + cas.dot(err_abs,err_abs) # cost term
+        objective = objective_reg/(N-1) # normalize with window length
+
+        # Solver
+        opti.minimize(objective)
+        opti.solver('ipopt',{"print_time":True,"expand":True},{'gamma_theta':1e-12,'max_iter':200,'tol':1e-4,'print_level':5,'ma57_automatic_scaling':'no','linear_solver':'mumps'})
+             
+if __name__ == "__main__":
+
+    OCP = OCP_calc_pose(N=100, rms_error_traj=10**-3)
